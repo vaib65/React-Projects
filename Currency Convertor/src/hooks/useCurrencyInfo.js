a1d5c881b7dfa142a7e15adc6fd50e2df02562fe
vaib65 <vhadke.02@gmail.com>
2025-06-24 19:42:06 +0530

    Add files via upload

---
+++ Currency Convertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,13 @@
+import { useState,useEffect } from "react";
+function useCurrencyInfo(currency) {
+    const [data,setData]=useState({})
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+        .then((res) => res.json())
+        .then((res) => setData[currency])
+        console.log(data);
+    }, [currency])
+    return data;
+}
+
+export default useCurrencyInfo;
